83f2dde9d5d777de624d3cb7a273c3d5ed87b418
JiHongYuan <1206429202@qq.com>
2022-04-13 16:50:03 +0800

    feat[word-cloud]: 查询一周消息,更友好提示

--- src/main/java/org/github/word/cloud/WordCloudCommand.java
+++ src/main/java/org/github/word/cloud/WordCloudCommand.java
@@ -13,6 +13,7 @@ import org.github.palace.bot.data.MybatisContext;
 import org.github.palace.bot.data.message.mapper.MessageMapper;
 
 import java.io.File;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -21,17 +22,28 @@ import java.util.List;
  */
 public class WordCloudCommand extends SimpleCommand {
     public WordCloudCommand() {
-        super("查看个人词云", null, false, "查看个人词云");
+        super("查看词云", null, false, "查看词云");
     }
 
     @CommandHandler
     public void handler(CommandSender commandSender, MessageSource messageSource, PlainText plainText) {
         MessageMapper messageMapper = MybatisContext.INSTANCE.get(MessageMapper.class);
-        List<String> messages = messageMapper.selectMessages(commandSender.getSubject().getId(), messageSource.getFromId());
 
-        File file = WordCloudUtil.gen(messages);
-        Image image = commandSender.getSubject().uploadImage(ExternalResource.create(file));
-        commandSender.sendMessage(new At(messageSource.getFromId()).plus(" ").plus(image));
+        Calendar calendar = Calendar.getInstance();
+        long end = calendar.getTimeInMillis();
+
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) - 7);
+        long start = calendar.getTimeInMillis();
+
+        List<String> messages = messageMapper.selectMessagesByCreateAt(commandSender.getSubject().getId(), messageSource.getFromId(), start, end);
+        if (!messages.isEmpty()) {
+            At at = new At(messageSource.getFromId());
+            commandSender.sendMessage(" 您一周内发言共" + messages.size() + "条, 请稍等词云正在制作中...");
+
+            File file = WordCloudUtil.gen(messages);
+            Image image = commandSender.getSubject().uploadImage(ExternalResource.create(file));
+            commandSender.sendMessage(at.plus(" ").plus(image));
+        }
     }
 
 }
